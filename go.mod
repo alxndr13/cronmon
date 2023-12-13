3257dde9f723617c837fe75110a8e265f2554b64
Alexander Huck <alexander.huck@inovex.de>
2023-12-13 08:23:23 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,20 @@
+module cronmon
+
+go 1.21
+
+require (
+	github.com/gen2brain/beeep v0.0.0-20230907135156-1a38885a97fc
+	github.com/urfave/cli/v2 v2.26.0
+	mvdan.cc/sh/v3 v3.7.0
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.3 // indirect
+	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/urfave/cli/v2"
+
+	"github.com/gen2brain/beeep"
+
+	"mvdan.cc/sh/v3/syntax"
+)
+
+var (
+	command       string = ""
+	cronName      string = ""
+	notifySuccess bool
+)
+
+func main() {
+
+	app := &cli.App{
+		Name:  "cronmon",
+		Usage: "runs a job and notifies according to your wishes 🔔",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "command",
+				Required:    true,
+				Destination: &command,
+				Aliases:     []string{"c"},
+			},
+			&cli.StringFlag{
+				Name:        "cron-name",
+				Required:    true,
+				Destination: &cronName,
+				Aliases:     []string{"cn"},
+			},
+			&cli.BoolFlag{
+				Name:        "notify-success",
+				DefaultText: "notifies also when the command was successfully executed",
+				Destination: &notifySuccess,
+				Aliases:     []string{"success"},
+				Value:       false,
+			},
+		},
+		Action: func(*cli.Context) error {
+			r := strings.NewReader(command)
+			f, err := syntax.NewParser().Parse(r, "")
+			if err != nil {
+				log.Fatal(err)
+			}
+			var buf bytes.Buffer
+			err = syntax.NewPrinter().Print(&buf, f)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			ex := exec.Command("bash", "-c", buf.String())
+			ex.Stdout = os.Stdout
+			ex.Stderr = os.Stderr
+			err = ex.Run()
+			if err != nil {
+				if exitError, ok := err.(*exec.ExitError); ok {
+					exitCode := exitError.ExitCode()
+					fmt.Println("Command exited with non-zero status:", exitCode)
+					beeep.Alert("Cronmon", fmt.Sprintf("running CronJob '%s' failed", cronName), "")
+				}
+				return nil
+			}
+			if notifySuccess {
+				beeep.Alert("Cronmon", fmt.Sprintf("running CronJob '%s' was successful", cronName), "")
+			}
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
